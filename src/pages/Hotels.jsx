1055a400010c637abac3a20371d903444a2e3e25
Nicolas Cohen <nicolascohen24@gmail.com>
2022-05-19 06:00:47 -0300

    Little Fixes, and Adder Button

--- src/pages/Hotels.jsx
+++ src/pages/Hotels.jsx
@@ -8,6 +8,8 @@ import { Pagination } from '../components/Pagination'
 
 import { endPoints } from '../services/main'
 
+import { PlusIcon } from '@heroicons/react/solid'
+
 const Hotels = () => {
   const hotels = useFetch(endPoints.hotels.allHotels)
 
@@ -24,6 +26,24 @@ const Hotels = () => {
   return (
     <div className='Hotels'>
       <Header />
+      <div className='max-w-7xl mx-auto sm:px-6 lg:px-8'>
+        <div className='lg:flex lg:items-center lg:justify-between mb-8'>
+          <div className='flex-1 min-w-0'>
+            <h2 className='text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate'>Lista de Usuarios</h2>
+          </div>
+          <div className='mt-5 flex lg:mt-0 lg:ml-4'>
+            <span className='sm:ml-3'>
+              <button
+                type='button'
+                className='inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500'
+              >
+                <PlusIcon className='-ml-1 mr-2 h-5 w-5' aria-hidden='true' />
+                Agregar Hoteles
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
       {
         hotels?.length > 6 && <Pagination
           thingsPerPage={hotelsPerPage}
