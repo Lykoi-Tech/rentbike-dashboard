e7ab0895d2e782a21a5e23f6a4a2b0f3b7ab45b7
Nicolas Cohen <nicolascohen24@gmail.com>
2022-05-19 12:13:32 -0300

    Buttons Disabled

--- src/pages/Suscriptors.jsx
+++ src/pages/Suscriptors.jsx
@@ -31,17 +31,6 @@ const Suscriptors = () => {
           <div className='flex-1 min-w-0'>
             <h2 className='text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate'>Lista de Usuarios</h2>
           </div>
-          <div className='mt-5 flex lg:mt-0 lg:ml-4'>
-            <span className='sm:ml-3'>
-              <button
-                type='button'
-                className='inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500'
-              >
-                <PlusIcon className='-ml-1 mr-2 h-5 w-5' aria-hidden='true' />
-                Agregar Suscriptor
-              </button>
-            </span>
-          </div>
         </div>
       </div>
       {
@@ -69,12 +58,6 @@ const Suscriptors = () => {
                     <th scope='col' className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
                       Estado
                     </th>
-                    <th scope='col' className='relative px-6 py-3'>
-                      <span className='sr-only'>Editar</span>
-                    </th>
-                    <th scope='col' className='relative px-6 py-3'>
-                      <span className='sr-only'>Eliminar</span>
-                    </th>
                   </tr>
                 </thead>
                 <tbody className='bg-white divide-y divide-gray-200'>
@@ -88,16 +71,6 @@ const Suscriptors = () => {
                         ? <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800'>Eliminado</span>
                         : <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>Activo</span>
                     }
-                      </td>
-                      <td className='px-6 py-4 whitespace-nowrap text-right text-sm font-medium'>
-                        <a href='/edit' className='text-indigo-600 hover:text-indigo-900'>
-                          Editar
-                        </a>
-                      </td>
-                      <td className='px-6 py-4 whitespace-nowrap text-right text-sm font-medium'>
-                        <a href='/edit' className='text-red-600 hover:text-red-900'>
-                          Eliminar
-                        </a>
                       </td>
                     </tr>
                   ))}
